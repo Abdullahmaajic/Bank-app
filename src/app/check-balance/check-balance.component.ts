41f3f0f134df943d21c85c10e4aa95dc0de9a3a8
Abdullahmaajic <abdullahmaajic@gmail.com>
2022-01-31 16:19:28 +0530

    bank-updated

--- src/app/check-balance/check-balance.component.ts
+++ src/app/check-balance/check-balance.component.ts
@@ -10,24 +10,24 @@ export class CheckBalanceComponent implements OnInit {
 
   ngOnInit(): void {}
 
-  verified:any = ' '
-  totalPayment:any = ' '
-  remaining:any = ' '
-  paymentDone:any = ' '
-  pay1:any = localStorage.getItem("phone")
-  pay2:any = localStorage.getItem("acc")
-  pay3:any = localStorage.getItem("upi")
-  pay4:any = localStorage.getItem("initial")
-  pay5:any = localStorage.getItem("remaining")
-  paymentType = ' '
+  verified: any = ' ';
+  totalPayment: any = ' ';
+  remaining: any = ' ';
+  paymentDone: any = ' ';
+  pay1: any = localStorage.getItem('phone');
+  pay2: any = localStorage.getItem('acc');
+  pay3: any = localStorage.getItem('upi');
+  pay4: any = localStorage.getItem('initial');
+  pay5: any = localStorage.getItem('remaining');
+  paymentType = ' ';
 
-  pay(pay:any){
-    this.paymentType = pay
+  pay(pay: any) {
+    this.paymentType = pay;
     // console.log(this.pay5)
   }
 
-  check(payment:any){
-    var pay = payment
+  check(payment: any) {
+    var pay = payment;
     // console.log(pay)
     // if(pay==this.pay1 ||pay==this.pay2 || pay==this.pay3){
     //   this.totalPayment = this.pay5
@@ -38,15 +38,14 @@ export class CheckBalanceComponent implements OnInit {
     //   alert("You entered the wrong inputs")
     // }
   }
-  checkbalance(pay:any){
-    if(pay==this.pay1 ||pay==this.pay2 || pay==this.pay3){
-      this.totalPayment = this.pay5
-      this.verified = "Verified"
-    this.remaining = this.pay5
-  }
-  else{
-      alert("You entered the wrong inputs")
-      this.remaining = ' '
+  checkbalance(pay: any) {
+    if (pay == this.pay1 || pay == this.pay2 || pay == this.pay3) {
+      this.totalPayment = this.pay5;
+      this.verified = 'Verified';
+      this.remaining = this.pay5;
+    } else {
+      alert('You entered the wrong inputs');
+      this.remaining = ' ';
     }
-}
+  }
 }
